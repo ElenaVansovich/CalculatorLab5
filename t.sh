b8c24491e863f1dab37d52f11ee6e2b581cd71c6
ElenaVansovich <elena.vansovich.96@mail.ru>
2016-12-06 12:11:19 +0300

    Add tests in file t.sh

--- t.sh
+++ t.sh
@@ -6,20 +6,21 @@ function test {
 	res=`sudo cat /dev/result`
 	echo "$1$2$3=${res}"
 	if [ "${res}" == "$4" ]; then
-		echo "OK"
+		echo "CORRECT"
 	else 
 		echo "NOT CORRECT"
 	fi
 }
 
 test 5 + 13 18
+test 15 - 30 -15
+test 10 - -10 20
+test 10 - 20 -10
+test 1 - 1 0
 test 12 / 2 6
 test 12 / -2 -6
 test 8 / 2 4
 test 10 '*' 4 40
 test 10 '*' -4 -40
 test -3 '*' -3 9
-test 15 - 30 -15
-test 10 - -10 20
-test 10 - 20 -10
-test 1 - 1 0
+
